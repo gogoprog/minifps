8f84486c13acbbde37b7b911ef831b6d00ca71fa
Gauthier Billot <gogoprog@gmail.com>
2024-10-02 22:43:18 +0200

    wip

--- src/Model.hx
+++ src/Model.hx
@@ -1,10 +1,12 @@
 package;
 
 class Model {
+    public var buffer:DataBuffer;
     public var vertexBuffer:js.html.webgl.Buffer;
     public var vertexCount:Int;
 
     public function new(buffer:DataBuffer) {
+        this.buffer = buffer;
         vertexBuffer = Renderer.createVertexBuffer(buffer);
         vertexCount = buffer.getCount();
         trace(vertexBuffer);
